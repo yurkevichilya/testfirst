f1e09b300a35bb8472eca105deb9ac6ddd399c0d
Ilya Yurkevich <i.yurkevich.vironit@vironit.ru>
2019-03-27 09:24:50 +0300

    Добавьте файлы проекта.

---
+++ testgit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
